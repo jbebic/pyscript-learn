321c2b18bce4336319c65c7db934bab9a10f54c5
Jovan Bebic <jzb@achillearesearch.com>
2023-04-26 12:47:19 -0400

    minor updates

--- test03.js
+++ test03.js
@@ -1,3 +1,4 @@
+var jzb;
 function ProcessFile(){
     let file = document.getElementById("file_input").files[0];
     if (file) {
@@ -5,8 +6,11 @@ function ProcessFile(){
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
             // document.getElementById("fileContents").innerHTML = evt.target.result;
-            plot_w_python = pyscript.interpreter.globals.get('plot_w_python')
-            plot_w_python(evt.target.result)
+            plot_w_python = pyscript.interpreter.globals.get('plot_w_python');
+            let temp = plot_w_python(evt.target.result);
+            // jzb = temp;
+            jzb = temp.toJs();
+            // jzb =  temp.getBuffer();
         }
         reader.onerror = function (evt) {
             document.getElementById("fileContents").innerHTML = "error reading file";
--- test03.py
+++ test03.py
@@ -1,10 +1,15 @@
-from io import StringIO
+from js import document, URL
+from io import StringIO, BytesIO
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf as dpdf
 
 def plot_w_python(data):
     datastr = StringIO(data)
     df1 = pd.read_csv(datastr)
+    buf = BytesIO()
+    pltPdf = dpdf.PdfPages('buf.pdf')
+
     display(df1.columns, target="display-write")
     fig, ax = plt.subplots()
     ax.plot(df1["t"], df1["v"])
@@ -12,4 +17,16 @@ def plot_w_python(data):
     ax.grid()
     plt.title("Plot from a local csv file")
     display(fig, target="graph-area", append=False)
-
+    pltPdf.savefig()
+    # plt.savefig(buf, format='pdf')
+    plt.close()
+    
+    pltPdf.close()
+    buf.seek(0)
+    file = URL.createObjectURL([buf])
+    # file = URL.createObjectURL(pltPdf)
+    filelink = Element("download-link")
+    filelink.element.innerHTML= "Jovan was here"
+    # fin = Element("file_input")
+    # filelink.element.href=fin.element.files[0]
+    return buf
